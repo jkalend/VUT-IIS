42a64d12ee1cae5571806ee987656237751d2f1e
Jaroslav Streit <Jarek.Streit@seznam.cz>
2023-10-24 23:33:40 +0200

    xddd

--- iot-iis/app/api/posts/route.ts
+++ iot-iis/app/api/posts/route.ts
@@ -1,24 +1,32 @@
 import prisma from '../../db'
+import {NextApiRequest, NextApiResponse} from "next";
 
-export async function POST() {
-    const res = await prisma.user.create({
-        data: {
-            name: 'Alice',
-            email: 'alice@prisma.io',
-        },
-    })
+export async function POST(req: Request) {
+    const {username, password, email} = await req.json();
+    console.log(username, password, email);
+    try {
+        const user = await prisma.user.create({
+            data: {
+                username: username,
+                password: password,
+                email: email,
+            },
+        })
 
-    const allUsers = await prisma.user.findMany({
-    })
-    console.dir(allUsers, { depth: null })
+        const allUsers = await prisma.user.findMany({})
+        console.dir(allUsers, {depth: null})
 
-    return Response.json(res)
+        return Response.json(user)
+    } catch (e) {
+        console.error("Couldn't create user:", e);
+        throw e;
+    }
 }
 
 export async function DELETE() {
     const res = await prisma.user.delete({
         where: {
-            email: 'alice@prisma.io',
+            email: 'idk@idk.com',
         }
     })
 
--- iot-iis/app/register/page.tsx
+++ iot-iis/app/register/page.tsx
@@ -1,6 +1,19 @@
 "use client";
 
+
 export default function Register() {
+
+    const sendForm = async() => {
+        let res = await fetch("/api/posts", {
+            method: "POST",
+            body: JSON.stringify({
+                name: "text",
+                surname:"text",
+            }),
+        });
+        console.log(res);
+    }
+
     return(
         <section>
             <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
@@ -9,10 +22,14 @@ export default function Register() {
                         <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-800 md:text-2xl dark:text-white">
                             Create a new account
                         </h1>
-                        <form className="space-y-4 md:space-y-6" action="#">
+                        <form className="space-y-4 md:space-y-6" onSubmit={sendForm}>
                             <div>
                                 <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-800 dark:text-white">Your email</label>
                                 <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required/>
+                            </div>
+                            <div>
+                                <label htmlFor="username" className="block mb-2 text-sm font-medium text-gray-800 dark:text-white">Your username</label>
+                                <input type="username" name="username" id="username" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required/>
                             </div>
                             <div>
                                 <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
