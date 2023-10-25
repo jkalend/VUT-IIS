01e32a02a1b9a018ccdd116c5dcc735513d5ffaa
kalen <jkalend@gmail.com>
2023-10-25 02:44:47 +0200

    made registration actually add users to the database

--- iot-iis/app/register/page.tsx
+++ iot-iis/app/register/page.tsx
@@ -1,7 +1,16 @@
 "use client";
 
+import {useState} from 'react';
+import {useRouter} from "next/navigation";
 
 export default function Register() {
+
+    let router = useRouter();
+
+    const [username, setUsername] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
 
     const sendForm = async() => {
         const res = await fetch(
@@ -9,14 +18,33 @@ export default function Register() {
             {
                 method: "POST",
                 body: JSON.stringify({
-                    username: "xyz",
-                    password: "123123",
-                    email: "idk@idk.com",
+                    username: username,
+                    password: password,
+                    email: email,
                 }),
             }
         )
         console.log(res);
+        router.push("/")
     }
+
+    const handleUsername = (event: any) => {
+        setUsername(event.target.value);
+
+        console.log('value is:', event.target.value);
+    };
+
+    const handleEmail = (event: any) => {
+        setEmail(event.target.value);
+
+        console.log('value is:', event.target.value);
+    };
+
+    const handlePassword = (event: any) => {
+        setPassword(event.target.value);
+
+        console.log('value is:', event.target.value);
+    };
 
     return(
         <section>
@@ -29,15 +57,15 @@ export default function Register() {
                         <form className="space-y-4 md:space-y-6" onSubmit={sendForm}>
                             <div>
                                 <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-800 dark:text-white">Your email</label>
-                                <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required/>
+                                <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" onChange={handleEmail} required/>
                             </div>
                             <div>
                                 <label htmlFor="username" className="block mb-2 text-sm font-medium text-gray-800 dark:text-white">Your username</label>
-                                <input type="username" name="username" id="username" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required/>
+                                <input type="username" name="username" id="username" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" onChange={handleUsername} required/>
                             </div>
                             <div>
                                 <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
-                                <input type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-orange-900 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-amber-400 dark:focus:border-orange-900" required/>
+                                <input type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-orange-900 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-amber-400 dark:focus:border-orange-900" onChange={handlePassword} required/>
                             </div>
                             <button type="submit" className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Sign up</button>
                         </form>
