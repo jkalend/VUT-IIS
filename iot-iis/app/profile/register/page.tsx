258d103685a1ae7481edf9c6a7c6c2192b70669b
kalen <jkalend@gmail.com>
2023-11-24 14:54:12 +0100

    weeee

--- iot-iis/app/profile/register/page.tsx
+++ iot-iis/app/profile/register/page.tsx
@@ -9,7 +9,6 @@ export default function Register() {
     let router = useRouter();
 
     const [username, setUsername] = useState('');
-    const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
 
@@ -72,10 +71,6 @@ export default function Register() {
                             Create a new account
                         </h1>
                         <form className="space-y-4 md:space-y-6" onSubmit={sendForm}>
-                            <div>
-                                <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-800 dark:text-white">Your email</label>
-                                <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" onChange={handleEmail} required/>
-                            </div>
                             <div>
                                 <label htmlFor="username" className="block mb-2 text-sm font-medium text-gray-800 dark:text-white">Your username</label>
                                 <input type="username" name="username" id="username" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" onChange={handleUsername} required/>
