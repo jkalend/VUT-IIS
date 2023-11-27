f8c2116f6835d5b2acd3f81474b1c947c3739e43
Tereza Kubincova <t.kubincova@gmail.com>
2023-11-27 16:30:43 +0100

    fixed broker

--- iot-iis/app/api/broker/route.ts
+++ iot-iis/app/api/broker/route.ts
@@ -2,6 +2,7 @@
 import prisma from "@/app/db";
 import {NextRequest, NextResponse} from "next/server";
 import jwt from "jsonwebtoken"
+import bcrypt from 'bcrypt'
 
 // POST - set new recent value for device using broker
 export const POST = async (request: NextRequest) => {
@@ -11,11 +12,22 @@ export const POST = async (request: NextRequest) => {
         // TODO: authenticate broker using JWT (toto spravim este ja)
         let authenticated = false;
         let data = jwt.decode (req.token, {complete: true})
-        if (data.payload.username == "cultsauce" && data.payload.password == "cultsauce")
-            authenticated = true;
-
+        let broker_user = await prisma.user.findUnique({
+                where: {
+                    username: data.payload.username
+                },
+                select: {
+                    broker_flag: true,
+                    password: true
+                }
+            })
+        if (broker_user) {
+            let valid = await bcrypt.compare(data.payload.password, broker_user.password)
+            if (valid && broker_user.broker_flag == 1)
+                authenticated = true
+        }
         if (!authenticated)
-            return NextResponse.json("Could not authenticate broker", { status: 400 })
+            return NextResponse.json("Could not authenticate broker" + data.payload.username, { status: 400 })
 
         // fetch parameter with data.payload.paramId
         const ok_vals = await prisma.parameter.findUnique({
@@ -29,10 +41,13 @@ export const POST = async (request: NextRequest) => {
                 return NextResponse.json("Wrong values", { status: 400 });
         }
         // set recent value of device with deviceId, paramId from request to recent_value from request
-        const value = await prisma.value.findUnique({
+        const value = await prisma.value.findFirst({
             where: {
                 deviceId: Number(data.payload.deviceId),
                 parameterId: Number(data.payload.paramId)
+            },
+            select: {
+                valueId: true
             }
         });
         const new_value = await prisma.value.update({
@@ -46,7 +61,7 @@ export const POST = async (request: NextRequest) => {
         return NextResponse.json(new_value, { status: 200 });
     } catch (err) {
         console.log (err)
-        return NextResponse.json("Could not change recent value of device" + err, { status: 500 });
+        return NextResponse.json("Could not change recent value of device", { status: 500 });
     }
 
 };
